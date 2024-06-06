28ec52ebedb5d39f72341aeca398082f0d2a08a5
arinmakk <arinmakk@gmail.com>
2024-06-06 15:11:23 +0800

    hi

--- src/pages/flashcard.js
+++ src/pages/flashcard.js
@@ -4,8 +4,20 @@ import styles from '../styles/flashcard.module.css';
 function Flashcards(){
 
     return(
-        <div className={styles.flashcard}>
-            <p></p>
+        <div className={styles.flashcardContainer}>
+            <div className={styles.flashcard}>
+                <div className={styles.question}>
+                    who in your family is gay?
+                </div>
+
+                <div className={styles.options}>
+                    <div className='optionBox'>mother</div>
+                    <div className='optionBox'>father</div>
+                    <div className='optionBox'>sister</div>
+                    <div className='optionBox'>brother</div>
+
+                </div>
+            </div>
         </div>
     );
 }
