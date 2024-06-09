86313f7b22fad570c993c54f0cd76977b9d817b0
shihuifong <146155096+shihuifong@users.noreply.github.com>
2024-06-09 15:23:45 +0800

    corrected the flashcard.module.css

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React, {useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 // PAGES
-import About from './pages/others/About';
-import Features from './pages/others/Features';
-import Navbar from './pages/common/NavBar';
-import SignIn from './pages/others/SignIn';
-import Flashcards from './pages/Flashcard';
-import Home from './pages/home/Home';
+import About from "./pages/others/About";
+import Features from "./pages/others/Features";
+import Navbar from "./pages/common/NavBar";
+import SignIn from "./pages/others/SignIn";
+import Flashcards from "./pages/flashcard";
+import Home from "./pages/home/Home";
 
 // STYLES
-import './styles/App.css';
+import "./styles/App.css";
 
 // FUNCTIONS
-import checkLogInStatus from './utility/protect';
+import checkLogInStatus from "./utility/protect";
 
 function App() {
   const [IsLoggedIn, setLogInStatus] = useState(true);
-  
-  // I HAVENT FIGURED OUT THE CODE BELOW 
-  
+
+  // I HAVENT FIGURED OUT THE CODE BELOW
+
   // useEffect(() => {
   //   console.log("i am runnning");
   //   setLogInStatus(checkLogInStatus());
   //   console.log(IsLoggedIn);
   // }, [IsLoggedIn]);
-  
+
   return (
     <Router>
       <Navbar />
@@ -33,9 +38,9 @@ function App() {
         <Route path="/" element={<About />} />
         <Route path="/features" element={<Features />} />
         <Route path="/login" element={<SignIn />} />
-        <Route 
-          path="/home/*" 
-          element={IsLoggedIn ? <Home /> : <Navigate to='/login' />} // REDIRECT TO SIGN IN PAGE IF NOT LOGGED IN
+        <Route
+          path="/home/*"
+          element={IsLoggedIn ? <Home /> : <Navigate to="/login" />} // REDIRECT TO SIGN IN PAGE IF NOT LOGGED IN
         />
         <Route path="/flashcard" element={<Flashcards />} />
       </Routes>
--- src/pages/flashcard.js
+++ src/pages/flashcard.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import styles from '../styles/Flashcard.module.css'; 
+import React from "react";
+import styles from "../styles/flashcard.module.css";
 
-function Flashcards(){
+function Flashcards() {
+  return (
+    <div className={styles.flashcardContainer}>
+      <div className={styles.flashcard}>
+        <div className={styles.question}>who in your family is gay?</div>
 
-    return(
-        <div className={styles.flashcardContainer}>
-            <div className={styles.flashcard}>
-                <div className={styles.question}>
-                    who in your family is gay?
-                </div>
-
-                <div className={styles.options}>
-                    <div className='optionBox'>mother</div>
-                    <div className='optionBox'>father</div>
-                    <div className='optionBox'>sister</div>
-                    <div className='optionBox'>brother</div>
-
-                </div>
-            </div>
+        <div className={styles.options}>
+          <div className="optionBox">mother</div>
+          <div className="optionBox">father</div>
+          <div className="optionBox">sister</div>
+          <div className="optionBox">brother</div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Flashcards;
