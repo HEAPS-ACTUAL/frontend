58fa0322a897e8100a0c1ca1cb2b116721c723be
jerricknsc <jerricknsc@gmail.com>
2024-06-11 10:28:12 +0800

    Update QuizService.js

--- src/services (for backend)/QuizService.js
+++ src/services (for backend)/QuizService.js
@@ -2,18 +2,19 @@ import axiosInstance  from "../utility/axiosInstance";
 
 async function generateSampleQuestions(file){
     try{
+        console.log(file);
         const formData = new FormData();
         formData.append('file', file[0]);
-        console.log(formData);
+        // console.log(formData);
         
         const response = await axiosInstance({
             method: 'post',
             url: '/file/upload',
             // url: '/quiz/generateSampleQuestions',
-            body: formData
+            data: formData
         })
 
-        console.log(response);
+        // console.log(response);
     }
     catch(error){
         if(error.response){
