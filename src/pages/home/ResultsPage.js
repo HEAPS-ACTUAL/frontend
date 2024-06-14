610a6a147b29420623f78f798badb50bbfa2da63
arinmakk <arinmakk@gmail.com>
2024-06-14 21:52:23 +0800

    solve error

--- src/pages/home/ResultsPage.js
+++ src/pages/home/ResultsPage.js
@@ -9,28 +9,32 @@ const ResultsPage = () => {
 
     return (
         <div className={styles.QuizResultsContainer}>
-
-            <h1>Results</h1>
-
+            <div className={styles.Header}>Results</div>
             {questions.map((question) => (
-                
                 <div key={question.id} className={styles.QuestionBlock}>
-                    <h2 className={styles.Question}>{question.question}</h2>
+                    <h2 className={styles.Question}>{question.id}. {question.question}</h2>
                     <div className={styles.OptionsContainer}>
                         {question.options.map((option) => (
                             <div key={option} className={`${styles.Option} 
-                                ${option === userAnswers[question.id] ? styles.UserAnswer : ''} 
+                                ${option === userAnswers[question.id] ? styles.UserAnswer : ''}
                                 ${option === question.correctAnswer ? styles.CorrectAnswer : ''}`}>
                                 {option}
                             </div>
                         ))}
                     </div>
-                    
-                    <p>Explanation: {question.explanation}</p>
+                    <div className={styles.Explanation}>Explanation: {question.explanation}</div>
                 </div>
             ))}
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br> 
         </div>
     );
-}
+};
 
 export default ResultsPage;
