9ef362ebc7935c997825b2402683bce32718f822
arinmakk <arinmakk@gmail.com>
2024-06-17 10:54:31 +0800

    remove elements from navbar

--- src/pages/main/NavBar.js
+++ src/pages/main/NavBar.js
@@ -34,8 +34,7 @@ function Navbar() {
                 <Link to="/" className={styles.navLink}>About</Link>
                 <Link to="/features" className={styles.navLink}>Features</Link>
                 <Link to="/Home" className={styles.navLink}>Home</Link>
-                <Link to="/Quiz" className={styles.navLink}>quiz(temp)</Link>
-                <Link to="/LoadingBar" className={styles.navLink}>Loading</Link>
+          
             </div>
 
             <Link to="/login" className={styles.navLinkLogin}><button onClick={handleLogOut}>{logInOutButton}</button></Link>
