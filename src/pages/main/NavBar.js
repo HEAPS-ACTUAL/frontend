b62bd3f248ca1d8665e0c91c4b5bb89c298c87bb
jerricknsc <jerricknsc@gmail.com>
2024-06-18 14:56:42 +0800

    many changes
    - included animations for navbar
    - included animation when quizcard and side elements are clicked

--- src/pages/main/NavBar.js
+++ src/pages/main/NavBar.js
@@ -31,10 +31,15 @@ function Navbar() {
             </Link>
 
             <div className={styles.navItems}>
-                <Link to="/" className={styles.navLink}>About</Link>
-                <Link to="/features" className={styles.navLink}>Features</Link>
-                <Link to="/Home" className={styles.navLink}>Home</Link>
-          
+                <p>
+                    <Link to="/" className={styles.navLink}>About</Link>
+                </p>
+                <p>
+                    <Link to="/features" className={styles.navLink}>Features</Link>
+                </p>
+                <p>
+                    <Link to="/Home" className={styles.navLink}>Home</Link>
+                </p>
             </div>
 
             <Link to="/login" className={styles.navLinkLogin}><button onClick={handleLogOut}>{logInOutButton}</button></Link>
