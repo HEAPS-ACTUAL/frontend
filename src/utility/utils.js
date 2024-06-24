b6b94de37216ce3f6e55af5b890cbfbbb7e8a939
arinmakk <arinmakk@gmail.com>
2024-06-25 00:42:01 +0800

    utils.js

--- src/utility/utils.js
+++ src/utility/utils.js
@@ -1,27 +1,3 @@
 import axiosInstance from "./axiosInstance"; // Adjust the path based on your project structure
 
 
-export const saveScheduleToDB = async (startDate, endDate, examName) => {
-  try {
-    const response = await axiosInstance.post("/api/schedules", {
-      startDate,
-      endDate,
-      examName,
-    });
-
-    return response.data.scheduleId;
-  } catch (error) {
-    console.error("Error saving schedule to DB:", error);
-  }
-};
-
-export const saveRevisionDatesToDB = async (scheduleId, revisionDates) => {
-  try {
-    await axiosInstance.post("/api/revision-dates", {
-      scheduleId,
-      revisionDates,
-    });
-  } catch (error) {
-    console.error("Error saving revision dates to DB:", error);
-  }
-};
