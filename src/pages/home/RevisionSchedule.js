21a080effbccd4f16d3230cd0cd2be5bbfdbeb18
shihuifong <146155096+shihuifong@users.noreply.github.com>
2024-06-24 17:30:35 +0800

    replace with arin SR logic

--- src/pages/home/RevisionSchedule.js
+++ src/pages/home/RevisionSchedule.js
@@ -255,16 +255,60 @@ const CalendarFeature = () => {
   };
 
   const calculateSpacedRepetitionDates = (startDate, endDate) => {
-    // Your logic to calculate spaced repetition dates
-    const revisionDates = [];
-    // Example logic for spaced repetition
-    const oneDay = 24 * 60 * 60 * 1000;
-    let currentDate = startDate;
-    while (currentDate <= endDate) {
-      revisionDates.push(new Date(currentDate).toISOString().split("T")[0]);
-      currentDate = new Date(currentDate.getTime() + oneDay * 2); // Example: Every 2 days
+    const currentDate = new Date(startDate); // dont change this, need this for the while loop calculation
+    const currentDateCorrectFormat = currentDate.toISOString().split("T")[0]; // Format date to YYYY-MM-DD to push into reviewDates array
+    const reviewDates = [currentDateCorrectFormat];
+
+    let intervals;
+
+    if (endDate === null) {
+      intervals = [3, 6, 9, 12];
+
+      let IntervalIndex = 0;
+
+      while (IntervalIndex < intervals.length) {
+        currentDate.setDate(currentDate.getDate() + intervals[IntervalIndex]);
+        const formattedDate = currentDate.toISOString().split("T")[0]; // Format date to YYYY-MM-DD
+        reviewDates.push(formattedDate); // Store the formatted date
+        IntervalIndex++;
+      }
+    } else {
+      const end = new Date(endDate);
+
+      // ensure start date is before end date
+      if (currentDate >= end) {
+        throw new Error("Start date must be before End date"); // window alert for this
+      }
+
+      // Calculate the number of days between start date and end date
+      const daysBetween = Math.ceil(
+        (end - currentDate) / (1000 * 60 * 60 * 24)
+      );
+
+      console.log(daysBetween);
+
+      // set the intervals based on the number of days btwn startDate and endDate
+      if (daysBetween <= 7) {
+        intervals = [1, 2, 4, 6];
+      } else if (daysBetween <= 14) {
+        intervals = [2, 4, 7];
+      } else if (daysBetween <= 30) {
+        intervals = [4, 8, 15];
+      } else {
+        intervals = [7, 14, 28];
+      }
+
+      let IntervalIndex = 0;
+
+      while (currentDate < end && IntervalIndex < intervals.length) {
+        currentDate.setDate(currentDate.getDate() + intervals[IntervalIndex]);
+        const formattedDate = currentDate.toISOString().split("T")[0]; // Format date to YYYY-MM-DD
+        reviewDates.push(formattedDate); // Store the formatted date
+        IntervalIndex++;
+      }
     }
-    return revisionDates;
+
+    return reviewDates;
   };
 
   return (
