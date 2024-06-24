a8e52f0a2ca5d3a35ed51c893f77f949353e9d81
Isaidchia <ezyisaiah@gmail.com>
2024-06-24 19:05:30 +0800

    Update NavBar.js

--- src/pages/main/NavBar.js
+++ src/pages/main/NavBar.js
@@ -40,9 +40,6 @@ function Navbar() {
                 <p>
                     <Link to="/Home" className={styles.navLink}>Home</Link>
                 </p>
-                <p>
-                    <Link to="/Flashcard" className={styles.navLink}>flashcard temp</Link>
-                </p>
             </div>
 
             <Link to="/login" className={styles.navLinkLogin}><button onClick={handleLogOut}>{logInOutButton}</button></Link>
