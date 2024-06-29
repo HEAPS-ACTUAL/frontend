69428068b656e0736e3d7fc36620c05ffec41c49
chloetay <145341634+chloetqy@users.noreply.github.com>
2024-06-29 22:15:38 +0800

    arin's code

--- src/services (for backend)/SpacedRepetitionService.js
+++ src/services (for backend)/SpacedRepetitionService.js
@@ -1,11 +1,39 @@
 import axiosInstance from "../utility/axiosInstance";
 
-async function getRevisionDates(ScheduleId, RevisionDates) { // YET TO CHANGE, CAN BE USED NEXT TIME WHEN WANT TO RETRIEVE REVISION DATES TO SHOW ON THE CALENDAR
+async function GetExamDetailsForCalendar(scheduleID, examName, examColour, revisionDates) { 
     try {
         const response = await axiosInstance({
             method: "post",
-            url: "/schedule/getRevisionDates",
-            data: { ScheduleId },
+            url: "/schedule/GetExamDetailsForCalendar",
+            data: { scheduleID: scheduleID, examName: examName, examColour: examColour, revisionDates: revisionDates },
+        });
+        return response.data;
+    } catch (error) {
+     async function getRevisionDates(ScheduleId, RevisionDates) { // YET TO CHANGE, C
+}
+
+// once i click generate schedule on the fe, this function will be called and will send data to the be
+async function createNewExam(startDate, endDate, examName, examColour, arrayOfTestIDs){
+    try {
+        const response = await axiosInstance({
+            method: "post",
+            url: "/schedule/createNewExam",
+            data: { startDate: startDate, endDate: endDate, examName: examName, examColour: examColour, arrayOfTestIDs: arrayOfTestIDs }
+        });
+
+        // console.log(response.data);
+async function createNewEvent(startDate, endDate, eventName, eventColour, arrayO
+    }
+}
+
+// ask jerrick to check
+async function DeleteExistingExam(scheduleID){
+    console.log(scheduleID);
+    try {
+        const response = await axiosInstance({
+            method: "post",
+            url: "/schedule/DeleteExistingExam",
+            data: { scheduleID: scheduleID }
         });
         return response.data;
     } catch (error) {
@@ -13,20 +41,4 @@ async function getRevisionDates(ScheduleId, RevisionDates) { // YET TO CHANGE, C
     }
 }
 
-// once i click generate schedule on the fe, this function will be called and will send data to the be
-async function createNewEvent(startDate, endDate, eventName, eventColour, arrayOfTestIDs){
-    try {
-        const response = await axiosInstance({
-            method: "post",
-            url: "/schedule/createNewEvent",
-            data: { startDate: startDate, endDate: endDate, eventName: eventName, eventColour: eventColour, arrayOfTestIDs: arrayOfTestIDs }
-        });
-
-        // console.log(response.data);
-        return response.data; // retrieve success/ fail message from the backend
-    } catch (error) {
-        return error.response.data.message;
-    }
-}
-
-export { createNewEvent, getRevisionDates };
+export { createNewExam, GetExamDetailsForCalendar, DeleteExistingExam };
