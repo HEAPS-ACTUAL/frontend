7ad169f713f0438db317b2b2ddcb1e9dc2500073
jerricknsc <jerricknsc@gmail.com>
2024-07-11 14:40:57 +0800

    hi

--- src/services (for backend)/ProtectionService.js
+++ src/services (for backend)/ProtectionService.js
@@ -1,16 +1,16 @@
 function isLoggedIn() {
-  return !!sessionStorage.getItem("userEmail");
-  // return true; // UNCOMMENT THIS LINE AND COMMENT THE ABOVE TO BYPASS THE PROTECTION
+    return !!sessionStorage.getItem("userEmail");
+    // return true; // UNCOMMENT THIS LINE AND COMMENT THE ABOVE TO BYPASS THE PROTECTION
 }
 
 function handleLogOut() {
-  sessionStorage.clear();
-  window.dispatchEvent(new Event("logInOut"));
+    sessionStorage.clear();
+    window.dispatchEvent(new Event("logInOut"));
 }
 
 function handleLogIn(email) {
-  sessionStorage.setItem("userEmail", email);
-  window.dispatchEvent(new Event("logInOut"));
+    sessionStorage.setItem("userEmail", email);
+    window.dispatchEvent(new Event("logInOut"));
 }
 
 export { isLoggedIn, handleLogOut, handleLogIn };
