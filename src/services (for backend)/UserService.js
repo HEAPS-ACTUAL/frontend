e95fb877427c495e4cd3d609449c6ee969ff6386
jerricknsc <jerricknsc@gmail.com>
2024-07-15 09:38:38 +0800

    changed abit of formatting

--- src/services (for backend)/UserService.js
+++ src/services (for backend)/UserService.js
@@ -3,133 +3,118 @@
 import axiosInstance from "../utility/axiosInstance";
 
 async function authenticate(email, password) {
-  try {
-    const response = await axiosInstance({
-      method: "post",
-      url: "/user/authenticate/",
-      data: { email: email, password: password },
-    });
+    try {
+        const response = await axiosInstance({
+            method: "post",
+            url: "/user/authenticate",
+            data: { email: email, password: password },
+        });
 
-    return response.data.message;
-  } catch (error) {
-    if (error.response) {
-      return error.response.data.message;
+        return response.data.message;
+    } 
+    catch (error) {
+        if (error.response) {
+            return error.response.data.message;
+        }
     }
-  }
 }
 
 async function createNewUser(email, password, firstName, lastName, gender) {
-  try {
-    const response = await axiosInstance({
-      method: "post",
-      url: "/user/register/",
-      data: { email, password, firstName, lastName, gender },
-    });
+    try {
+        const response = await axiosInstance({
+            method: "post",
+            url: "/user/register/",
+            data: { email, password, firstName, lastName, gender },
+        });
 
-    return response.data.message;
-  } catch (error) {
-    if (error.response) {
-      return error.response.data.message;
+        return response.data.message;
+    } 
+    catch (error) {
+        if (error.response) {
+            return error.response.data.message;
+        }
     }
-  }
 }
 
 async function getUserByEmail(email) {
-  try {
-    const response = await axiosInstance({
-      method: "post",
-      url: "/user/profile/",
-      data: { email: email },
-    });
-    return response.data;
-  } catch (error) {
-    if (error.response) {
-      return error.message;
+    try {
+        const response = await axiosInstance({
+            method: "post",
+            url: "/user/profile/",
+            data: { email: email },
+        });
+        return response.data;
+    } 
+    catch (error) {
+        if (error.response) {
+            return error.message;
+        }
     }
-  }
 }
 
 async function getUserFirstName(email) {
-  const userFound = await getUserByEmail(email);
-  return userFound.FirstName;
+    const userFound = await getUserByEmail(email);
+    return userFound.FirstName;
 }
 
 async function getSalutation(email) {
-  const userFound = await getUserByEmail(email);
-  const gender = userFound.Gender;
-
-  if (gender === "F") {
-    return "Ms";
-  } else {
-    return "Mr";
-  }
-}
-
-async function getDateJoined(email) {
     const userFound = await getUserByEmail(email);
-    return userFound.DateTimeJoined;
-  }
+    const gender = userFound.Gender;
+
+    if (gender === "F") {
+        return "Ms";
+    } 
+    else {
+        return "Mr";
+    }
+}
 
 // Update User Details
 async function updateUserDetails({ email, firstName, lastName }) {
-  try {
-    console.log("Sending user details for update:", {
-      email,
-      firstName,
-      lastName,
-    }); // Debugging log
-    const response = await axiosInstance.put("/user/update/", {
-      email,
-      firstName,
-      lastName,
-    });
-    return response.data.message;
-  } catch (error) {
-    if (error.response) {
-      return error.response.data.message;
+    try {
+        console.log("Sending user details for update:", {email, firstName, lastName}); // Debugging log
+        const response = await axiosInstance.put("/user/update/", {
+            email,
+            firstName,
+            lastName,
+        });
+        return response.data.message;
+    } catch (error) {
+        if (error.response) {
+            return error.response.data.message;
+        }
     }
-  }
 }
 
 // Delete User Account
 async function deleteUserAccount(email) {
-  try {
-    const response = await axiosInstance.delete("/user/delete/", {
-      data: { email },
-    });
-    return response.data.message;
-  } catch (error) {
-    if (error.response) {
-      return error.response.data.message;
+    try {
+        const response = await axiosInstance.delete("/user/delete/", {
+            data: { email },
+        });
+        return response.data.message;
+    } catch (error) {
+        if (error.response) {
+            return error.response.data.message;
+        }
     }
-  }
 }
 
-async function checkUserIsVerified(email){
-    try{
+async function checkUserIsVerified(email) {
+    try {
         const response = await axiosInstance({
             method: "post",
             url: "/user/is-verified/",
-            data: {email: email}
+            data: { email: email }
         });
         return response.data;
     }
-    catch(error){
-        if(error.response){
+    catch (error) {
+        if (error.response) {
             return error.message;
         }
     }
-  }
+}
 
 
-export {
-  authenticate,
-  createNewUser,
-  getUserByEmail,
-  getUserFirstName,
-  getSalutation,
-  updateUserDetails,
-  deleteUserAccount,
-  getDateJoined,
-  checkUserIsVerified
-};
+export {authenticate, createNewUser, getUserByEmail, getUserFirstName, getSalutation, updateUserDetails, deleteUserAccount, checkUserIsVerified};
