d9b4a7dfb8aa89fc3c0ef7c4caf52e91af78e89d
jerricknsc <jerricknsc@gmail.com>
2024-07-17 18:33:12 +0800

    changed abit of logic of how the page works

--- src/pages/main/ProfilePage.js
+++ src/pages/main/ProfilePage.js
@@ -15,12 +15,12 @@ function ProfilePage() {
     const email = sessionStorage.getItem("userEmail");
 
     const [userDetails, setUserDetails] = useState({});
+    const [newUserDetails, setNewUserDetails] = useState({}); // THIS VARIABLE IS FOR DISPLAYING THE NEW NAME AS THE USER TYPES IN THEIR NEW NAME
     const [daysSinceCreation, setDaysSinceCreation] = useState(0);
     const [daysPercentage, setDayPercentage] = useState(0);
+    
+    const [changingName, setChangingName] = useState(false);
     const [changingPassword, setChangingPassword] = useState(false);
-    const [passwords, setPasswords] = useState({ currentPassword: '', newPassword: '', confirmPassword: '' });
-
-    const [editing, setEditing] = useState(false);
 
     useEffect(() => {
 
@@ -29,6 +29,7 @@ function ProfilePage() {
             // console.log("Fetched user details:", retrievedUser); // Debugging log
 
             setUserDetails(retrievedUser);
+            setNewUserDetails(retrievedUser);
 
             // Calculate days since account creation
             const creationDate = new Date(retrievedUser.DateTimeJoined);
@@ -55,11 +56,17 @@ function ProfilePage() {
 
     function handleChange(event) {
         const { name, value } = event.target;
-        setUserDetails((prev) => ({ ...prev, [name]: value }));
+        setNewUserDetails((prev) => ({ ...prev, [name]: value }));
     }
 
-    async function handleSave() {
-        const { FirstName, LastName } = userDetails;
+    function handleCancel(){
+        setNewUserDetails(userDetails); // SET NEW 
+        setChangingName(false);
+        setChangingPassword(false);
+    }
+
+    async function handleNameSave() {
+        const { FirstName, LastName } = newUserDetails;
 
         if (FirstName.trim() === '' || LastName.trim() === '') {
             alert('Fields cannot be empty!');
@@ -67,18 +74,17 @@ function ProfilePage() {
         else {
             const message = await updateUserDetails(email, FirstName, LastName); // hashedPassword, inputPassword and newPassword are "null" by default when updating firstName and/or lastName. Refer to updateUserDetails in UserServices.js
 
+            if (message === 'User details updated successfully.'){
+                setUserDetails(newUserDetails); // UPDATE PREVIOUS USER DETAILS TO THE NEW ONE WITHOUT HAVING TO RELOAD THE PAGE
+            }
+
             alert(message);
-            setEditing(false);
+            setChangingName(false);
         }
     }
 
     async function handlePasswordSave() {
-        const { HashedPassword, InputPassword, NewPassword, ConfirmPassword } = userDetails;
-
-        // console.log(`hashed pw: ${HashedPassword}`)
-        // console.log(`input pw: ${InputPassword}`)
-        // console.log(`new pw: ${NewPassword}`)
-        // console.log(`confirm pw: ${ConfirmPassword}`)
+        const { HashedPassword, InputPassword, NewPassword, ConfirmPassword } = newUserDetails;
 
         if (!InputPassword || !NewPassword || !ConfirmPassword) {
             alert('Fields cannot be empty!');
@@ -114,60 +120,64 @@ function ProfilePage() {
                     <div className="profile-image-background"></div>
                     <img src={getProfileImage(userDetails.Gender)} alt={`${userDetails.Gender} profile`} className="profile-image" />
                 </div>
-                <div className="profile-info">
-                    <div className="name">
-                        {userDetails.FirstName} {userDetails.LastName}
-                        {editing ? '' : <FaEdit className="edit-icon" onClick={() => setEditing(true)} />}
-                    </div>
-                    {editing
-                        ? (<div>
-                            <input type="text" name="FirstName" onChange={handleChange} placeholder={userDetails.FirstName} />
-                            <input type="text" name="LastName" onChange={handleChange} placeholder={userDetails.LastName} />
 
-                            <button onClick={handleSave}>Save Changes</button>
-                            <button onClick={() => setEditing(false)}>Cancel</button>
-                        </div>)
-                        : changingPassword
-                            ? (<div className="password-change-container">
+                <div className="profile-info">
+
+                    {/* IF NOT CHANGING NAME OR CHANGING PASSWORD */}
+                    {!changingName && !changingPassword
+                        ? <div>
+                            <div className="name">
+                                {userDetails.FirstName} {userDetails.LastName}
+                                <FaEdit className="edit-icon" onClick={() => setChangingName(true)} />
+                            </div>
+
+                            <div className="email">{userDetails.Email}</div>
+
+                            <div className="progress-container">
+                                <div className="progress-bar">
+                                    <CircularProgressbar value={daysPercentage} text={`${daysSinceCreation}`}
+                                        styles={buildStyles({
+                                            rotation: 0.25,
+                                            strokeLinecap: 'butt',
+                                            textSize: '35px',
+                                            pathTransitionDuration: 0.5,
+                                            pathColor: `rgba(70, 99, 172, 0.7)`,
+                                            trailColor: `rgb(201, 200, 198, 1)`,
+                                            backgroundColor: '#3e98c7',
+                                            textColor: `rgb(70, 99, 172, 0.7)`,
+                                        })} />
+                                </div>
+                                <div className="days-since">Days since you joined quizDaddy!</div>
+                            </div>
+                            <button onClick={() => setChangingPassword(true)}>Change Password</button>
+                            <button onClick={handleDeleteAccount}>Delete Account</button>
+                        </div>
+
+                        // IF CHANGING NAME
+                        : changingName && !changingPassword
+                            ? <div>
+                                <div className="name">{newUserDetails.FirstName} {newUserDetails.LastName}</div>
+
+                                <input type="text" name="FirstName" onChange={handleChange} placeholder={userDetails.FirstName} />
+                                <input type="text" name="LastName" onChange={handleChange} placeholder={userDetails.LastName} />
+
+                                <button onClick={handleNameSave}>Save Changes</button>
+                                <button onClick={handleCancel}>Cancel</button>
+                            </div>
+                            
+                            // IF CHANGING PASSWORD
+                            : <div className="password-change-container">
                                 <input type="password" name="InputPassword" onChange={handleChange} placeholder="Current Password" />
                                 <input type="password" name="NewPassword" onChange={handleChange} placeholder="New Password" />
                                 <input type="password" name="ConfirmPassword" onChange={handleChange} placeholder="Confirm Password" />
+
                                 <button onClick={handlePasswordSave}>Save Password</button>
-                                <button onClick={() => setChangingPassword(false)}>Cancel</button>
-                            </div>)
-                            : (<div>
-                                <p className="email">{userDetails.Email}</p>
-
-                                <div className="progress-container">
-                                    <div className="progress-bar">
-                                        <CircularProgressbar className='progress-bar' value={daysSinceCreation} text={`${daysSinceCreation}`}
-                                            styles={buildStyles({
-                                                rotation: 0.25,
-                                                strokeLinecap: 'butt',
-                                                textSize: '35px',
-                                                pathTransitionDuration: 0.5,
-                                                pathColor: `rgba(70, 99, 172, 0.7), ${daysPercentage})`,
-                                                textColor: '#f88',
-                                                trailColor: `rgb(201, 200, 198, 1)`,
-                                                backgroundColor: '#3e98c7',
-                                                textColor: `rgb(70, 99, 172, 0.7)`,
-
-
-                                            })} />
-                                    </div>
-
-                                    <div className="days-since">Days since you joined quizDaddy</div>
-                                </div>
-
-                                <button onClick={() => setChangingPassword(true)}>Change Password</button>
-                                <button onClick={handleDeleteAccount}>Delete Account</button>
-                            </div>)
-
-
+                                <button onClick={handleCancel}>Cancel</button>
+                            </div>
                     }
                 </div>
             </div>
-        </div >
+        </div>
     );
 }
 
