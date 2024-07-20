c6d1d7b6d4411f413a105dca168ed11c827898ab
jerricknsc <jerricknsc@gmail.com>
2024-07-20 15:35:03 +0800

    added back "no events today"

--- src/pages/home/Calendar/DayModal.js
+++ src/pages/home/Calendar/DayModal.js
@@ -26,18 +26,21 @@ function DayModal({ date, events, handleEventClick, handleDeleteClicked }) {
                 <div className={styles.modalHeader}>Today's Events</div>
                 <div className={styles.modalDate}>{formattedDate}</div>
                 <div className={`${styles.eventsContainer} ${events.length === 0 ? styles.noEvents : styles.haveEvents}`} >
-                    {events.map((event) => (
-                        <div key={event.id} className={styles.eventItem}>
+                    {events.length !== 0 
+                        ? events.map((event) => (
+                            <div key={event.id} className={styles.eventItem}>
 
-                            <button className={styles.eventTitle} onClick={() => handleEventClick(event.id[0])} >
-                                {event.title}
-                            </button>
+                                <button className={styles.eventTitle} onClick={() => handleEventClick(event.id[0])} >
+                                    {event.title}
+                                </button>
 
-                            <button onClick={() => handleDeleteClicked(event)} className={styles.deleteButton} >
-                                <FaTrashCan style={{ opacity: 0.5 }} />
-                            </button>
-                        </div>
-                    ))}
+                                <button onClick={() => handleDeleteClicked(event)} className={styles.deleteButton} >
+                                    <FaTrashCan style={{ opacity: 0.5 }} />
+                                </button>
+                            </div>
+                        ))
+                        : <div> -- No events today -- </div>
+                }
                 </div>
             </div>
         </div>
