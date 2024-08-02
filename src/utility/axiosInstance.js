b165553bc167d02f2254ddd1bf8eff9bb40a5a0c
Isaidchia <ezyisaiah@gmail.com>
2024-08-03 00:43:10 +0800

    Update axiosInstance.js
    Added prototype EC2 instance

--- src/utility/axiosInstance.js
+++ src/utility/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BE_IP_ADDRESS = '54.255.215.236';
+const BE_IP_ADDRESS = '54.255.215.236'; // production
+const BE_PROTO_IP = '18.139.224.111'; // prototype
 const localhost = 'localhost';
 
 const axiosInstance = axios.create({
