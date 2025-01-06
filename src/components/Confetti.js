140aac93cf8ff736b4a06a33ac9ad3017ee6f274
jerricknsc <jerricknsc@gmail.com>
2025-01-06 18:38:01 +0800

    Update Confetti.js

--- src/components/Confetti.js
+++ src/components/Confetti.js
@@ -1,9 +1,7 @@
 import React from 'react'
-// import { useWindowSize } from 'react-use'
 import Confetti from 'react-confetti'
 
 export default () => {
-    // const { width, height } = useWindowSize()
 
     return (
         <Confetti
